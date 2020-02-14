55739d1d3efd4c7187f414a9ca9769e6df897245
michael.landry <michael.landry@pps.io>
2020-02-14 09:36:49 -0500

    feb

---
+++ scripts/readme.md
@@ -0,0 +1,5 @@
+THis is my personal collection scripts to make my life easier.
+It lives in ~/scripts
+You store work scripts in ~/source/scripts
+also wd will point at this as personalscripts
+
--- workcomputer.bash
+++ workcomputer.bash
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #start of work machine
 
 #work alias
@@ -13,23 +14,6 @@ echo "workcomputer.bash running"
 # source '.wd'
 source ~/.wd
  
-#fiserv stuff
-# # export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home"
-# export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_211.jdk/Contents/Home"
-# export PATH=$JAVA_HOME/bin:$PATH
-
-# export ANT_HOME="/opt/onlinemessenger/apache-ant-1.9.13"
-# export PATH=$ANT_HOME/bin:$PATH
-
-# export GRADLE_HOME="/opt/onlinemessenger/gradle-3.5.1"
-# export PATH=$GRADLE_HOME/bin:$PATH
-
-# export GROOVY_HOME="/opt/onlinemessenger/groovy-1.8.9"
-# export PATH=$GROOVY_HOME/bin:$PATH
-
-# export PATH=${PATH}:/Library/PostgreSQL/9.6/bin
-
-#end of fiserv ---------------------------------
 #PPS stuff
 
 # Username
@@ -131,6 +115,17 @@ function kget() {
 # subl $F
 # sleep .3  # give subl a little time to open the file
 # rm -f $F  # file will be deleted as soon as subl closes it
+
+
+# KM Stuff
+# function ss() {
+#     osascript -e 'tell application "Keyboard Maestro Engine" to do script "FCAD61F1-352E-4DD3-8489-DA7A38281021"'
+# }
+
+function oss() {
+    osascript -e 'tell application "Keyboard Maestro Engine" to do script "1FB6E951-25A3-4CFE-84C0-FAF0D9B9D68E"'
+}
+
 
 
 
